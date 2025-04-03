ffeba8eec78a79beac66b4ac699e08dbb48f848e
matheus <matheusoliveira1991@hotmail.com>
2025-04-03 14:18:04 -0300

    status responses enum

---
+++ src/main/java/br/com/meetime/hubspot/enums/StatusHubSpotApiEnum.java
@@ -0,0 +1,27 @@
+package br.com.meetime.hubspot.enums;
+
+public enum StatusHubSpotApiEnum {
+
+    CLIENTID_OBRIGATORIO(1, "Parametro clientId obrigatorio"),
+    ERRO_SERIALIZACAO(2, "Erro ao realizar serializacao do Objeto, favor consultar administrador de sistemas"),
+    ACCESS_TOKEN_NULL(3, "AccessToken retornado vazio, verifique se os parametros estao corretos ou consultar administrador de sistemas"),
+    CLIENTSECRET_OBRIGATORIO(4, "Parametro clientSecret obrigatorio"),
+    CODE_OBRIGATORIO(5, "Parametro code obrigatorio");
+
+
+    private final int statusCode;
+    private final String status;
+
+    StatusHubSpotApiEnum(int statusCode, String status) {
+        this.statusCode = statusCode;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
