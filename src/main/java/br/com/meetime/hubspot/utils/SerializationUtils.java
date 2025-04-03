40bfbc116b6be9968a0fc15f1d70dc2943fcc085
matheus <matheusoliveira1991@hotmail.com>
2025-04-03 17:06:27 -0300

    obj para json

--- src/main/java/br/com/meetime/hubspot/utils/SerializationUtils.java
+++ src/main/java/br/com/meetime/hubspot/utils/SerializationUtils.java
@@ -1,5 +1,6 @@
 package br.com.meetime.hubspot.utils;
 
+import br.com.meetime.hubspot.dto.request.AccountHubSpotDTO;
 import br.com.meetime.hubspot.exceptions.SerializationUtilsException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,15 @@ public class SerializationUtils {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             return objectMapper.readValue(json, clazz);
+        } catch (Exception e) {
+            throw new SerializationUtilsException(e.getMessage());
+        }
+    }
+
+    public String objectToJson(AccountHubSpotDTO accountHubSpotDTO) throws SerializationUtilsException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(accountHubSpotDTO);
         } catch (Exception e) {
             throw new SerializationUtilsException(e.getMessage());
         }
