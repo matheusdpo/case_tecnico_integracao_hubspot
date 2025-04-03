13ee0dae283797212d8780dea66e166b6f8ba02a
matheus <matheusoliveira1991@hotmail.com>
2025-04-03 14:18:11 -0300

    exception serialization

---
+++ src/main/java/br/com/meetime/hubspot/exceptions/SerializationUtilsException.java
@@ -0,0 +1,7 @@
+package br.com.meetime.hubspot.exceptions;
+
+public class SerializationUtilsException extends RuntimeException {
+    public SerializationUtilsException(String message) {
+        super(message);
+    }
+}
