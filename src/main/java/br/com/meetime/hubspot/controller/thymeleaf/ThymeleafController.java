c9d0919890882c8d006d6a1f0627d7057c98a092
matheus <matheusoliveira1991@hotmail.com>
2025-04-05 07:01:04 -0300

    adiciodnado controller

--- src/main/java/br/com/meetime/hubspot/controller/thymeleaf/ThymeleafController.java
+++ src/main/java/br/com/meetime/hubspot/controller/thymeleaf/ThymeleafController.java
@@ -1,10 +1,9 @@
-package br.com.meetime.hubspot.controller;
+package br.com.meetime.hubspot.controller.thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
 @Controller
