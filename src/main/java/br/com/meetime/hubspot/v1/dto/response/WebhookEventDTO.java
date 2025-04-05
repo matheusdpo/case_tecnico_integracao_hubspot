c42b0d19360440ca52656b523008ea491cac86de
matheus <matheusoliveira1991@hotmail.com>
2025-04-05 08:35:13 -0300

    criado dto para padronizacao

---
+++ src/main/java/br/com/meetime/hubspot/v1/dto/response/WebhookEventDTO.java
@@ -0,0 +1,68 @@
+package br.com.meetime.hubspot.v1.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.ZonedDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WebhookEventDTO {
+    private String eventType;
+    private String propertyName;
+    private boolean active;
+    private String id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    private ZonedDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    private ZonedDateTime updatedAt;
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(ZonedDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ZonedDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(ZonedDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
