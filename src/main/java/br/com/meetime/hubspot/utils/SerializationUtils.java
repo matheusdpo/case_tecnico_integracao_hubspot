82894767b973b2a83b6c4b32c0e0df28cdd56068
matheus <matheusoliveira1991@hotmail.com>
2025-04-05 07:00:42 -0300

    serializado datas

--- src/main/java/br/com/meetime/hubspot/utils/SerializationUtils.java
+++ src/main/java/br/com/meetime/hubspot/utils/SerializationUtils.java
@@ -3,6 +3,8 @@ package br.com.meetime.hubspot.utils;
 import br.com.meetime.hubspot.dto.request.AccountHubSpotDTO;
 import br.com.meetime.hubspot.exceptions.SerializationUtilsException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,9 @@ public class SerializationUtils {
 
     public <T> T jsonToObject(String json, Class<T> clazz) throws SerializationUtilsException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         try {
             return objectMapper.readValue(json, clazz);
         } catch (Exception e) {
@@ -17,10 +22,10 @@ public class SerializationUtils {
         }
     }
 
-    public String objectToJson(AccountHubSpotDTO accountHubSpotDTO) throws SerializationUtilsException {
+    public String objectToJson(Object obj) throws SerializationUtilsException {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.writeValueAsString(accountHubSpotDTO);
+            return objectMapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new SerializationUtilsException(e.getMessage());
         }
